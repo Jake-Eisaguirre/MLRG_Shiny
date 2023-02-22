b318620034dea296e371b4eae4df540db698abcb
jake-eisaguirre <eisaguirre@bren.ucsb.edu>
2023-02-22 09:00:47 -0800

    update for new user

--- MLRG_Shinyapp/global.R
+++ MLRG_Shinyapp/global.R
@@ -42,28 +42,28 @@ shinyOptions(cache = cachem::cache_disk("./app_cache"))
 #shinyOptions(cache = cachem::cache_mem(max_size = 1000e6))
 
 #Bd data read in
-bd_data <- read_csv(here("data/bd_data.csv"))
+bd_data <- read_csv(here("MLRG_Shinyapp/data/bd_data.csv"))
 
 #read in wilderness shape files
-shape <- read_sf(here("data/wilderness_shapes/wilderness.shp")) 
+shape <- read_sf(here("MLRG_Shinyapp/data/wilderness_shapes/wilderness.shp")) 
 
 #read in water types
-water <- read_csv(here("data/water_type.csv")) %>% 
+water <- read_csv(here("MLRG_Shinyapp/data/water_type.csv")) %>% 
   rename(id = lake_id)
 
 #ves data read in
-ves_data <- read_csv(here("data/ves_data.csv")) %>% 
+ves_data <- read_csv(here("MLRG_Shinyapp/data/ves_data.csv")) %>% 
   left_join(bd_data) %>% 
   left_join(water)
 
 # read in bd_plot data with month_year
-bd_plot <- read_csv(here("data/bd_plot.csv"))
+bd_plot <- read_csv(here("MLRG_Shinyapp/data/bd_plot.csv"))
 
 # read in ves_plot data with year_month
-ves_plot <- read_csv(here("data/ves_plot.csv"))
+ves_plot <- read_csv(here("MLRG_Shinyapp/data/ves_plot.csv"))
 
 # all visit data for map
-all_visits <- read_csv(here( "data/all_visits.csv")) %>% 
+all_visits <- read_csv(here( "MLRG_Shinyapp/data/all_visits.csv")) %>% 
   left_join(water, by = c("site_id" = "id")) %>%
   anti_join(ves_data, by = c("site_id" = "id"))
 
@@ -72,37 +72,37 @@ all_visits <- read_csv(here( "data/all_visits.csv")) %>%
 
 
 # Site table
-site <- read_csv(here("data/site.csv"))
+site <- read_csv(here("MLRG_Shinyapp/data/site.csv"))
 
 # visit table
-visit <- read_csv(here("data/visit.csv"))
+visit <- read_csv(here("MLRG_Shinyapp/data/visit.csv"))
 
 # survey table
-survey <- read_csv(here("data/survey.csv"))
+survey <- read_csv(here("MLRG_Shinyapp/data/survey.csv"))
 
 # capture table
-capture <- read_csv(here("data/capture.csv"))
+capture <- read_csv(here("MLRG_Shinyapp/data/capture.csv"))
 
 # full capture data set
-full_capture <- read_csv(here("data/full_capture.csv"))
+full_capture <- read_csv(here("MLRG_Shinyapp/data/full_capture.csv"))
 
 # bd load table
-bd_load_table <- read_csv(here("data/bd_load_table.csv"))
+bd_load_table <- read_csv(here("MLRG_Shinyapp/data/bd_load_table.csv"))
 
 # VES table
-ves_table <- read_csv(here("data/ves_table.csv"))
+ves_table <- read_csv(here("MLRG_Shinyapp/data/ves_table.csv"))
 
 # full Ves data
-full_ves <- read_csv(here("data/full_ves.csv"))
+full_ves <- read_csv(here("MLRG_Shinyapp/data/full_ves.csv"))
 
 # relocate table
-relocate_table <- read_csv(here("data/relocate_table.csv"), col_types = cols(collect_siteid = "c"))
+relocate_table <- read_csv(here("MLRG_Shinyapp/data/relocate_table.csv"), col_types = cols(collect_siteid = "c"))
 
 # frog relocate table
-relocate_frog_table <- read_csv(here("data/relocate_frog_table.csv"))
+relocate_frog_table <- read_csv(here("MLRG_Shinyapp/data/relocate_frog_table.csv"))
 
 # full CMR table
-full_cmr <- read_csv(here("data/full_cmr.csv"), col_types = cols(collect_siteid = "c"))
+full_cmr <- read_csv(here("MLRG_Shinyapp/data/full_cmr.csv"), col_types = cols(collect_siteid = "c"))
 
 
 inactivity <- "function idleTimer() {
